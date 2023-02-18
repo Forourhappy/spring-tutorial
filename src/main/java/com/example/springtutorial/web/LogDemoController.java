31474dbca28b8566654cd7c556f1ae8db36dcb9d
Forourhappy <foroh1504@gmail.com>
2023-02-19 00:03:50 +0900

    bean scope 학습

--- src/main/java/com/example/springtutorial/web/LogDemoController.java
+++ src/main/java/com/example/springtutorial/web/LogDemoController.java
@@ -14,12 +14,11 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURI().toString();
 //        이 부분은 인터셉터로 구현하는 것이 더 좋다
         myLogger.setRequestURL(requestURL);
